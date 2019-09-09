8d39fad9d35f4c611149ae68769ab6442af4a86f
Luc Brulet <s3798189@w8408193.cs.rmit.edu.au>
2019-09-09 20:40:13 +1000

    add model

--- IOSApp/IOSApp/Model/Muscles.swift
+++ IOSApp/IOSApp/Model/Muscles.swift
@@ -17,24 +17,3 @@ struct Muscle {
         self.label = label
     }
 }
-
-struct Muscles {
-    private var muscles:[Muscle] = []
-    private var currentImage:Int = 0
-    
-    init() {
-        populateMuscles()
-    }
-    
-    private mutating func populateMuscles() {
-        muscles.append(Muscle(image: "back2", label: "B A C K"))
-        muscles.append(Muscle(image: "biceps2", label: "B I C E P S"))
-    }
-    
-    public mutating func getNextMuscle() -> Muscle {
-        if currentImage == 1 {currentImage = 0}
-        else {currentImage += 1}
-        return muscles[currentImage]
-    }
-    
-}
