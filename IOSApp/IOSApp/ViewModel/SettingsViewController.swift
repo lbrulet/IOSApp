2c98c88c3fd53fa59bf6667b75b14ac4a838996d
Lancia Romain <s3797461@w8408192.cs.rmit.edu.au>
2019-09-10 21:13:53 +1000

    feat: add button link to alert dialog

---
+++ IOSApp/IOSApp/ViewModel/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  IOSApp
+//
+//  Created by Lancia Romain on 10/9/19.
+//  Copyright © 2019 Luc Brulet. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Alert", message:
+            "Are you sure you want to delete your account ? All your datas will be lost", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "No", style: .default))
+        alertController.addAction(UIAlertAction(title: "Yes ", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
